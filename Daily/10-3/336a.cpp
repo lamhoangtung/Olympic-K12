c5e0ddb845cb7cdcb108f5509e3b1d727882f590
lamhoangtung <lamhoangtung@users.noreply.github.com>
2018-03-10 17:05:19 +0700

    Bug fixed

--- Daily/10-3/336a.cpp
+++ Daily/10-3/336a.cpp
@@ -8,5 +8,5 @@ int main(void){
   if (x>0 && y>0) cout << "0 " << x+y << " " << x+y << " 0";
   else if (x<0 && y>0) cout << x-y << " 0 0 " << y-x;
   else if (x<0 && y<0) cout << x+y << " 0 0 " << x+y;
-  else if (x>0 && y<0) cout << "0 " << y-x << x-y << " 0";
+  else if (x>0 && y<0) cout << "0 " << y-x << " " << x-y << " 0";
 }
