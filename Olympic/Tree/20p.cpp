225304fd8bd503924573359f55d1b3bf57d7b56b
lamhoangtung <lamhoangtung.vz@gmail.com>
2018-07-13 19:13:04 +0700

    Applied tree data structure

--- Olympic/Tree/20p.cpp
+++ Olympic/Tree/20p.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 int n,a[1000000];
 long long tree[4000000];
 
@@ -23,4 +27,22 @@ long long query(int l,int r, int node, int low, int high){
   if (l==low && l==high) return tree[node];
   if (low>r || high<l) return 0;
   return query(l,(l+r)/2,2*node,low,high) + query((((l+r)/2)+1),r,2*node+1,low,high);
+}
+
+int main(){
+  int k;
+  cin >> n >> k;
+  for (int i=1;i<=n;i++){
+    cin >> a[i];
+  }
+  build(1,n,1);
+  for (int i=1;i<=k;i++){
+    int type,x,y;
+    cin >> type >> x >> y;
+    if (type == 1)
+      update(1,n,x,y,1);
+    if (type == 0)
+      cout << query(1,n,1,x,y) << "\n";
+  }
+  return 0;
 }
